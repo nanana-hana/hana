c38acd2ca2a47f74c40c6cb597992fe683a65745
nanana-hana <hanaaisyahhilman@gmail.com>
2025-05-21 14:08:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("selamat datang di web informatika")
+st.title("selamat datang di web informatika ")
 st.write(
     "ngoding seru sama pa hendri"
     st.image("IMG_20250521_140236_999.jpg", width=200)
